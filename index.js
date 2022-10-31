bee1cf0becea687cbf5482fa0791831aa4c7f818
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-10-31 16:41:21 -0500

    commit tree f4df5505612d1b757423c06ca5804633ab918295

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue < 400) {
+  return "This one is on me!"
+  } else if (someValue < 2000) {
+  return "That will be twenty bucks."
+  } else if (someValue < 2500) {
+  return "I will gladly take your thirty bucks."
+  } else if (someValue <= 2501)
+  return "No can do."  
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let thank;
+  switch (tip) {
+    case 'generous': 
+      thank = 'Thank you so much.';
+      break;
+    case 'not as generous': 
+      thank = 'Thank you.';
+      break;
+    default: 
+      thank = 'Bye.';
+      break;
+  }
+  return thank;
 }
